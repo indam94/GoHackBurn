615da9d3c6343dc016bdb32d1dc61d4ca91e476d
kimtaehun <rlaxogns6744@gmail.com>
2017-09-26 12:44:33 +0900

    playerctrl 스크립트 작성(앞뒤좌우이동가능), animation 기본 설정함

--- PocketShooter/Assets/02. Scripts/PlayerCtrl.cs
+++ PocketShooter/Assets/02. Scripts/PlayerCtrl.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerctrl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+[System.Serializable]
+public class Anim
+{
+    public AnimationClip idle;
+    public AnimationClip runFoward;
+    public AnimationClip runBackward;
+    public AnimationClip runRight;
+    public AnimationClip runLeft;
+}
+public class PlayerCtrl : MonoBehaviour {
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    private Transform tr;
+    public float moveSpeed = 10.0f;
+    public float rotSpeed = 100.0f;
+
+    // Use this for initialization
+    void Start () {
+        tr = GetComponent<Transform>();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        h = Input.GetAxis ("Horizontal"); // InputManager의 "Horizontal"에 설정된 값을 입력했을때 -1 ~ +1 까지의 값을 반환
+        v = Input.GetAxis ("Vertical"); // InputManager의 "Vertical"에 설정된 값을 입력했을때 -1 ~ +1 까지의 값을 반환
+        
+        // 디버깅 정보를 텍스트 형태로 console 뷰에 표시
+        Debug.Log("H=" + h.ToString());
+        Debug.Log("V=" + v.ToString());
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        tr.Translate(moveDir * moveSpeed * Time.deltaTime, Space.Self);
+
+        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+    }
 }
