679ddaa3ae8bc836bbbf64cf0aa2e672bb87cf66
indam944 <na940107@naver.com>
2017-10-10 22:32:52 +0900

    총알 발사 기능 구현

---
+++ PocketShooter/Assets/02. Scripts/BallCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCtrl : MonoBehaviour {
+
+	//총알의 파괴력
+	public int damage = 20;
+	//총알 발사 속도
+	public float speed = 1000.0f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().AddForce(transform.forward * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
