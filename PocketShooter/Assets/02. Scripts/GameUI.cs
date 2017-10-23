642736da02dca921269b420dae6e0d262fedba28
DESKTOP-6N8RRFH\min <mtj2741@gmail.com>
2017-10-23 13:53:03 +0900

    UI 작업

---
+++ PocketShooter/Assets/02. Scripts/GameUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+    public Text txtScore;
+    //누적 점수
+    private int totScore = 0;
+    
+    void Start()
+    {
+        DispScore(0);
+    }
+
+    public void DispScore(int score)
+    {
+        totScore += score;
+        txtScore.text = "SCORE <color=#ff0000>" + totScore.ToString() + "</color>";
+    }
+}
